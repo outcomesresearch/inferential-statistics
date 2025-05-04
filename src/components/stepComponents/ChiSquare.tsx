2125075169da15420fce1f75533d4487ed337261
jpiccirillo <piccirilloj1@gmail.com>
2025-05-04 13:55:22 -0500

    Update ChiSquare.tsx

--- src/components/stepComponents/ChiSquare.tsx
+++ src/components/stepComponents/ChiSquare.tsx
@@ -18,48 +18,27 @@ const Component = () => {
         </Text>
         .
       </Text>
-
       <Text>
         The{" "}
         <Text fw="600" span>
           Chi-Square test of goodness-of-fit
         </Text>{" "}
-        checks if the distribution of a single categorical variable matches an
-        expected distribution. For example, a study might check if a treatment
-        is equally effective across three age groups. The test compares observed
-        success rates with expected ones to see if the treatment distribution
-        differs from the expected one.
+        examines whether an observed frequency distribution fits an
+        expected/hypothesized distribution. It compares the observed frequencies
+        of a single variable with the expected frequencies under the specific
+        expected distribution.
       </Text>
-
       <Text>
         The{" "}
         <Text fw="600" span>
           Chi-Square test of independence
         </Text>{" "}
-        examines if two categorical variables are associated. For instance, a
-        study might investigate whether smoking status is related to lung
-        disease. The test compares the observed frequencies of smokers and
-        non-smokers with and without lung disease to see if there’s an
-        association.
-      </Text>
-
-      <Text>
-        When comparing two groups, such as smokers vs. non-smokers, the
-        Chi-Square test checks if the incidence of lung disease is significantly
-        different between the two groups. With three or more groups, such as
-        comparing the effectiveness of different drugs across multiple age
-        groups, the test shows if any significant differences exist across the
-        groups, but further tests are needed to determine which specific groups
-        differ.
-      </Text>
-
-      <Text>
-        In both cases, the test assumes the sample size is large enough for
-        valid results. If the sample size is small, alternative tests, such as{" "}
-        <Text fw="600" span>
-          Fisher’s Exact Test
-        </Text>
-        , may be used.
+        is used to examine the association between two categorical variables by
+        comparing the observed frequencies with the expected frequencies. It
+        determines whether differences between expected and observed values are
+        due to chance or because the variables are dependent on each other.
+        Requires that no cells have expected frequency less than 1 and no more
+        than 25% of the cells have expected frequencies less than 5.
       </Text>
     </>
   );
