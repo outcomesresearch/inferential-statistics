5ffb320ba2b47b278ebb4cecc9a5a766edefdb26
jpiccirillo <piccirilloj1@gmail.com>
2025-05-04 13:44:19 -0500

    Update BinaryLogistic.tsx

--- src/components/stepComponents/BinaryLogistic.tsx
+++ src/components/stepComponents/BinaryLogistic.tsx
@@ -11,12 +11,15 @@ const Component = () => {
         models the probability or likelihood of an event occurring based on the
         predictor variables.
       </Text>
-
       <Text>
         For example, it can be used to predict the likelihood of a patient
         having a disease (presence/absence) based on their age, gender, and
-        other relevant factors. The outcome variable is encoded as 0 or 1,
-        representing the absence or presence of the event, respectively.
+        other relevant factors. The exponentiated coefficients of Negative
+        Binomial Regression represent the Odds Ratio for experiencing the
+        outcome of interest for every one-unit increase in the corresponding
+        predictor variable, after controlling for other variables in the model.
+        Odds ratios quantify the strength of the relationship of each predictor
+        with the outcome after controlling for other variables in the model.
       </Text>
     </>
   );
